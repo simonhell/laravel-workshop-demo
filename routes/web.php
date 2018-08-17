66fd318f34acbcaffda100bb451793924ed0628d
Simon Hell <simon@deckweiss.at>
2018-08-17 11:06:35 +0200

    Added route with parameter

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('/hello', function() {
     return 'Hello World!';
 });
+
+Route::get('/hello/{name}', function($name) {
+   return 'Hello ' . $name . '!';
+});
