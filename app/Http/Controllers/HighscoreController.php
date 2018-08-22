cb86f4af4b02732995808eea117b9ada2ecb44c7
betzalex <alexander.betz@notrufnoe.at>
2018-08-22 13:56:20 +0200

    Added Validation to highscores and updated the frontend highscore table with real values

--- app/Http/Controllers/HighscoreController.php
+++ app/Http/Controllers/HighscoreController.php
@@ -3,24 +3,77 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
 use App\User;
 use App\Drink;
 
 class HighscoreController extends Controller
 {
     public function index() {
-        return view('highscores.index')->with(['users' => User::all()]);
+        $users = User::all()->map(function($user) {
+            $drinks = $user->drinks;
+            $totalAmountMl = $drinks->reduce(function($total, $drink) { return $total + $drink->amount; }, 0);
+            $totalAmountAlcoholMl = $drinks->reduce(function($total, $drink) {
+                return $total + (($drink->amount / 100) * doubleval($drink->alcohol));
+            }, 0);
+            $totalAmountAlcoholMl = round($totalAmountAlcoholMl);
+
+            return (object)[
+                'id' => $user->id,
+                'first_name' => $user->first_name,
+                'last_name' => $user->last_name,
+                'age' => $user->age,
+                'drinks' => count($drinks),
+                'total_amount_ml' => $totalAmountMl,
+                'total_amount_alcohol_ml' => $totalAmountAlcoholMl
+            ];
+        });
+
+        $users = $users->sortByDesc('total_amount_alcohol_ml')->values()->all();
+
+        return view('highscores.index')->with(['users' => $users]);
     }
 
     public function saveDrink(Request $request) {
-        $drink = new Drink();
-        $drink->name = $request->name;
-        $drink->amount = $request->amount;
-        $drink->alcohol = $request->alcohol;
+        $rules = [
+            'user_id' => 'required|integer',
+            'name' => 'required|string|min:2',
+            'amount' => 'required|numeric|min:20',
+            'alcohol' => 'required|numeric|min:2|max:100'
+        ];
 
-        $user = User::findOrFail($request->user_id);
-        $user->drinks()->save($drink);
+        $messages = [
+            'user_id.required' => 'Please select a user',
+            'user_id.integer' => 'Invalid user',
+            'name.required' => 'Please enter a name for your drink',
+            'name.string' => 'Please enter a valid name for your drink',
+            'name.min' => 'Your drinks name must have at least 2 characters',
+            'amount.required' => 'Please enter the amount you want to drink',
+            'amount.numeric' => 'Still okay drinking? Don\'t think so',
+            'amount.min' => 'Is your drink really that hard?',
+            'alcohol.required' => 'Let us know what a man you are',
+            'alcohol.numeric' => 'Are you sure this is a valid alcohol content?',
+            'alcohol.min' => 'What is this? I don\'t consider your drink as the right one for this game',
+            'alcohol.max' => 'Don\'t you think you\'re going a little too far?'
+        ];
 
-        return redirect()->route('highscores.index');
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if($validator->fails()) {
+            return redirect()
+                ->route('highscores.index')
+                ->withErrors($validator)
+                ->withInput();
+        } else {
+            $drink = new Drink();
+            $drink->name = $request->name;
+            $drink->amount = $request->amount;
+            $drink->alcohol = $request->alcohol;
+
+            $user = User::findOrFail($request->user_id);
+            $user->drinks()->save($drink);
+
+            return redirect()->route('highscores.index');
+        }
     }
 }
--- resources/views/highscores/index.blade.php
+++ resources/views/highscores/index.blade.php
@@ -11,40 +11,32 @@
                 <th>Last Name</th>
                 <th>Age</th>
                 <th>Drinks</th>
-                <th>total amount (ml)</th>
-                <th>total amount alcohol (ml)</th>
+                <th>Total Amount (ml)</th>
+                <th>Total Amount of Alcohol (ml)</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>1</td>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>22</td>
-                <td>10</td>
-                <td>1000</td>
-                <td>50</td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>22</td>
-                <td>10</td>
-                <td>1000</td>
-                <td>50</td>
-            </tr>
-            <tr>
-                <td>3</td>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>22</td>
-                <td>10</td>
-                <td>1000</td>
-                <td>50</td>
-            </tr>
+            @foreach($users as $user)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $user->first_name }}</td>
+                    <td>{{ $user->last_name }}</td>
+                    <td>{{ $user->age }}</td>
+                    <td>{{ $user->drinks }}</td>
+                    <td>{{ $user->total_amount_ml }}</td>
+                    <td>{{ $user->total_amount_alcohol_ml }}</td>
+                </tr>
+            @endforeach
+
+            @if(count($users) <= 0)
+                <tr>
+                    <td colspan="7">No one got drunk in the last couple moments</td>
+                </tr>
+            @endif
         </tbody>
     </table>
+
+    @if(count($users) > 0)
     <hr>
     <h1>Add drink</h1>
     <form action="{{ route('highscores.saveDrink') }}" method="POST">
@@ -54,33 +46,48 @@
             <div class="col-md-6">
                 <div class="form-group">
                     <label><b>Select User</b></label>
-                    <select class="form-control" name="user_id">
-                        <option>
+                    <select class="form-control" name="user_id" value="{{ old('user_id') }}">
+                        <option disabled selected>
                             Select User...
                         </option>
                         @foreach ($users as $user)
-                        <option value="{{ $user->id }}">
-                            {{ $user->first_name ." " .$user->last_name }}
-                        </option>
+                            @if (intval(old('user_id')) === $user->id)
+                                <option value="{{ $user->id }}" selected>{{ $user->first_name ." " .$user->last_name }}</option>
+                            @else
+                                <option value="{{ $user->id }}">{{ $user->first_name ." " .$user->last_name }}</option>
+                            @endif
                         @endforeach
                     </select>
+                    @if($errors->has('user_id'))
+                        <small class="form-text text-danger">{{ implode(' ', $errors->get('user_id')) }}</small>
+                    @endif
                 </div>
                 <div class="form-group">
                     <label><b>Drink name</b></label>
-                    <input type="text" name="name" class="form-control" placeholder="Drink Name">
+                    <input type="text" name="name" class="form-control" placeholder="Drink Name" value="{{ old('name') }}">
+                    @if($errors->has('name'))
+                        <small class="form-text text-danger">{{ implode(' ', $errors->get('name')) }}</small>
+                    @endif
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="form-group">
                     <label><b>Amount (ml)</b></label>
-                    <input type="number" name="amount" class="form-control" placeholder="Amount">
+                    <input type="number" name="amount" min="0" class="form-control" placeholder="Amount" value="{{ old('amount') }}">
+                    @if($errors->has('amount'))
+                        <small class="form-text text-danger">{{ implode(' ', $errors->get('amount')) }}</small>
+                    @endif
                 </div>
                 <div class="form-group">
                     <label><b>Alocohol content (%)</b></label>
-                    <input type="number" name="alcohol" class="form-control" placeholder="Alcohol content">
+                    <input type="number" name="alcohol" min="0" max="100" step="0.1" class="form-control" placeholder="Alcohol content" value="{{ old('alcohol') }}">
+                    @if($errors->has('alcohol'))
+                        <small class="form-text text-danger">{{ implode(' ', $errors->get('alcohol')) }}</small>
+                    @endif
                 </div>
             </div>
         </div>
         <button type="submit" class="btn btn-primary btn-lg">Add drink</button>
     </form>
+    @endif
 @endsection
